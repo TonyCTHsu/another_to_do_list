dd6217aba95a7cc7622fff1c36c8d49d899d2c28
Tony Hsu <tonyc.t.hsu@gmail.com>
2019-02-21 03:34:45 +0800

    Increase seed combinations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-List.create(name: 'Programming languages').tap do |l|
-  ListItem.create(description: 'Ruby', list: l)
-  ListItem.create(description: 'Javascript', list: l)
+List.create(name: 'Published list with published items').tap do |l|
+  l.list_items.create(description: 'Real Madrid')
+  l.list_items.create(description: 'Barcelona')
 end
 
-List.create(name: 'Common data').tap do |l|
-  ListItem.create(description: 'Postgres', list: l)
-  ListItem.create(description: 'Redis', list: l)
-  ListItem.create(description: 'Elasticsearch', list: l)
-  ListItem.create(description: 'Kafka', list: l)
+List.create(name: 'Published list with partial published items').tap do |l|
+  l.list_items.create(description: 'Man City')
+  l.list_items.create(description: 'Man Utd', publish: false)
+end
+
+List.create(name: 'Published list with unpublished items').tap do |l|
+  l.list_items.create(description: 'Arsenal', publish: false)
+  l.list_items.create(description: 'Chelsea', publish: false)
+end
+
+List.create(name: 'Unpublished list with published items', publish: false).tap do |l|
+  l.list_items.create(description: 'Liverpool')
+  l.list_items.create(description: 'Tottenham Hotspurs')
+end
+
+List.create(name: 'Unpublished list partial published items', publish: false).tap do |l|
+  l.list_items.create(description: 'Bayern Munich')
+  l.list_items.create(description: 'Juventus', publish: false)
+end
+
+List.create(name: 'Unpublished list with unpublished items', publish: false).tap do |l|
+  l.list_items.create(description: 'AC Milan', publish: false)
+  l.list_items.create(description: 'Inter Milan', publish: false)
 end
